3eaa56a5d28bd0370f3afc4b5434fba7286cfe42
Arti M <artismarti@gmail.com>
2015-03-21 20:52:12 +0000

    Completes Arrays Checkpoint.

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d){
+  var newArray = [a,b,c,d];
+  return newArray;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array){
+  var lastElementIndex = array.length - 1;
+  var newArray = [array[0], array[lastElementIndex]];
+  return newArray;
+};
