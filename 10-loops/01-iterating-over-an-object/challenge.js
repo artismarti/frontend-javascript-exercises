23400bec0b59bb8c281b7ed1b9a35da125fa54a9
Arti M <artismarti@gmail.com>
2015-04-01 21:15:31 +0100

    Completes Loops exercise.

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,9 +1,23 @@
 module.exports.getKeys = function(object){
-  
+  var arr = [];
+  for (key in object) {
+    arr.push(key);
+  }
+  return arr;
 };
 
-module.exports.getValues = function(){
+module.exports.getValues = function(obj){
+  var arr = [];
+  for (key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
 };
 
-module.exports.objectToArray = function(){
+module.exports.objectToArray = function(object){
+  var arr = [];
+  for (key in object) {
+    arr.push(key + " is " + object[key]);
+  }
+  return arr;
 };
--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,26 @@
-module.exports.copy = undefined;
+module.exports.copy = function(obj){
+  var newObj = {};
+  for (key in obj) {
+    newObj[key] = obj[key];
+  };
+  return newObj;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src){
+  for (key in src) {
+    dest[key] = src[key];
+  }
+  return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj, arr){
+  var newArr = [];
+  var i = arr.length;
+  for (key in obj) {
+    newArr.push(key);
+  };
+  while(i--) {
+    if (arr[i] !== newArr[i]) return false;
+  }
+  return true;
+};
